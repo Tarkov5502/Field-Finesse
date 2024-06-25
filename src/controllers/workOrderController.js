e68f4bb27d138c0ead2b36264f4815952f6369d0
Samuel Elder <s.j.elder888@gmail.com>
2024-06-24 22:01:19 -0400

    More changes to backend

--- src/controllers/workOrderController.js
+++ src/controllers/workOrderController.js
@@ -1,9 +1,15 @@
 const WorkOrder = require('../models/workOrder');
+const Schedule = require('../models/schedule');
+const Shipping = require('../models/shipping');
+const Cost = require('../models/cost');
 
 exports.createWorkOrder = async (req, res) => {
   try {
     const workOrder = await WorkOrder.create(req.body);
-    res.status(201).json(workOrder);
+    const schedule = await Schedule.create({ ...req.body.schedule, work_order_id: workOrder.id });
+    const shipping = await Shipping.create({ ...req.body.shipping, work_order_id: workOrder.id });
+    const cost = await Cost.create({ ...req.body.cost, work_order_id: workOrder.id });
+    res.status(201).json({ workOrder, schedule, shipping, cost });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
