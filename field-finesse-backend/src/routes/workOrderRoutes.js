2b7dfd0ce49a3718f2a0d1b26607a99991631bb8
Samuel Elder <s.j.elder888@gmail.com>
2024-06-29 22:59:54 -0400

    Corrected workOrderRoutes.js

--- field-finesse-backend/src/routes/workOrderRoutes.js
+++ field-finesse-backend/src/routes/workOrderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const workOrderController = require('../../controllers/workOrderController');
+const workOrderController = require('../controllers/workOrderController');
 
 router.post('/', workOrderController.createWorkOrder);
 router.get('/', workOrderController.getWorkOrders);
